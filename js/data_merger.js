fec15cd163a132564f9486bf91bb26845d51d5c3
OHTSUKA Ko-hei <kochizufan@gmail.com>
2020-07-12 14:41:32 +0900

    新規調査の画像追加

--- js/data_merger.js
+++ js/data_merger.js
@@ -2,7 +2,15 @@ const fs = require('fs-extra');
 
 const pois = fs.readJsonSync('../pois.geojson');
 const files = fs.readJsonSync('../files.geojson');
+/*const refs = fs.readJsonSync('../refs.geojson');
 
+const refsBuffer = {};
+refs.features.map((curr) => {
+    if (!refsBuffer[curr.properties.poiid]) {
+        refsBuffer[curr.properties.poiid] = [];
+    }
+    refsBuffer[curr.properties.poiid].push(curr.properties);
+});*/
 const filesBuffer = {};
 files.features.map((curr) => {
     if (!filesBuffer[curr.properties.poiid]) {
@@ -11,9 +19,27 @@ files.features.map((curr) => {
     filesBuffer[curr.properties.poiid].push(curr.properties);
 });
 pois.features.map((poi) => {
+    /*if (refsBuffer[poi.properties.fid]) {
+        poi.properties.refs = refsBuffer[poi.properties.fid];
+    }*/
     if (filesBuffer[poi.properties.fid]) {
         poi.properties.files = filesBuffer[poi.properties.fid];
+        const primary = poi.properties.files.reduce((prev, curr) => {
+            if (!prev) return curr;
+            if (poi.properties.primary_image == curr.fid) return curr;
+            return prev;
+        }, null);
+        poi.properties.path = primary.path;
+        poi.properties.small_thumbnail = primary.small_thumbnail;
+        poi.properties.mid_thumbnail = primary.mid_thumbnail;
+    } else {
+        poi.properties.files = [];
+        poi.properties.path = null;
+        poi.properties.small_thumbnail = null;
+        poi.properties.mid_thumbnail = null;
     }
+    delete poi.properties.primary_image;
+    delete poi.properties.brushup;
 });
 fs.writeJsonSync('../tatebayashi_stones.geojson', pois, {
     spaces: '  '
