8d0d00df05931a2a584f1121f2a8f48c301a2485
Kohei Otsuka <kochizufan@gmail.com>
2021-06-13 13:26:56 +0900

    data_mergeを調整

--- js/data_merger.js
+++ js/data_merger.js
@@ -1,46 +1,38 @@
 const fs = require('fs-extra');
 
 const pois = fs.readJsonSync('../pois.geojson');
-const files = fs.readJsonSync('../files.geojson');
-/*const refs = fs.readJsonSync('../refs.geojson');
+const images = fs.readJsonSync('../images.geojson');
+const refs = fs.readJsonSync('../refs.geojson');
+const books = fs.readJsonSync('../books.geojson');
 
-const refsBuffer = {};
-refs.features.map((curr) => {
-    if (!refsBuffer[curr.properties.poiid]) {
-        refsBuffer[curr.properties.poiid] = [];
-    }
-    refsBuffer[curr.properties.poiid].push(curr.properties);
-});*/
-const filesBuffer = {};
-files.features.map((curr) => {
-    if (!filesBuffer[curr.properties.poiid]) {
-        filesBuffer[curr.properties.poiid] = [];
-    }
-    filesBuffer[curr.properties.poiid].push(curr.properties);
-});
-pois.features.map((poi) => {
-    /*if (refsBuffer[poi.properties.fid]) {
-        poi.properties.refs = refsBuffer[poi.properties.fid];
-    }*/
-    if (filesBuffer[poi.properties.fid]) {
-        poi.properties.files = filesBuffer[poi.properties.fid];
-        const primary = poi.properties.files.reduce((prev, curr) => {
-            if (!prev) return curr;
-            if (poi.properties.primary_image == curr.fid) return curr;
-            return prev;
-        }, null);
-        poi.properties.path = primary.path;
-        poi.properties.small_thumbnail = primary.small_thumbnail;
-        poi.properties.mid_thumbnail = primary.mid_thumbnail;
-    } else {
-        poi.properties.files = [];
-        poi.properties.path = null;
-        poi.properties.small_thumbnail = null;
-        poi.properties.mid_thumbnail = null;
-    }
-    delete poi.properties.primary_image;
-    delete poi.properties.brushup;
+pois.features.forEach((poi) => {
+  const props = poi.properties;
+  const poiid = props.fid;
+  props.images = images.features.map(x => x.properties).filter((image) => {
+    return image.poi === poiid;
+  }).map((image) => {
+    const ret = Object.assign({}, image);
+    delete ret.poi;
+    delete ret.fid;
+    return ret;
+  });
+
+  props.books = refs.features.map(x => x.properties).filter((ref) => {
+    return ref.poi === poiid;
+  }).map((ref) => {
+    const ret = Object.assign({}, ref);
+    const book = books.features.map(x => x.properties).reduce((prev, book) => {
+      return ref.book === book.fid ? book : prev;
+    }, undefined);
+    delete ret.poi;
+    delete ret.fid;
+    delete ret.book
+    ret.name = book.name;
+    ret.editor = book.editor;
+    ret.publishedAt = book.publishedAt;
+    return ret;
+  });
 });
 fs.writeJsonSync('../tatebayashi_stones.geojson', pois, {
-    spaces: '  '
+  spaces: '  '
 });
