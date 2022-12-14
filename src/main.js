32cddd216fbb01262997cdd8484ef82b0fd60599
kochizufan <kochizufan@gmail.com>
2022-12-15 04:43:49 +0900

    動くようになった

--- src/main.js
+++ src/main.js
@@ -57,13 +57,75 @@ let newEditMarker;
 let currentTwitSubmitter;
 let currentTwitCanceller;
 
-
 fetch(geoBuf).then(async (response) => {
+  const confirmed = L.layerGroup([]);
+  const nonconfirmed = L.layerGroup([]);
+  const container = document.querySelector('#container');
+  const pane = container.querySelector("#poipane");
+  const close_button = pane.querySelector(".poipane-close-button");
+  close_button.addEventListener("click", closePoiPane);
+
   for await (let feature of flatgeobuf.deserialize(response.body)) {
-    console.log(feature);
+    feature = Quyuan.templateExtractor({
+      geojson: feature,
+      templates: {
+        pin: iconTemplate,
+        html: popupHtmlTemplate,
+      }
+    });
+    if (feature.geometry) {
+      const icons = feature.result.pin.split(',');
+      const iconUrl = icons[0];
+      const width = parseInt(icons[1]);
+      const height = parseInt(icons[2]);
+      const iconOptions = {
+        iconUrl,
+        iconSize: [width, height],
+        iconAnchor: [width / 2, height],
+        popupAnchor: [0, -1 * height],
+      };
+      // source file coordinates are ordered by lnglat, but should be latlng.
+      const marker = L.marker(feature.geometry.coordinates.reverse(), {
+        icon: L.icon(iconOptions),
+      });
+      marker.html = feature.result.html;
+      marker.name = feature.properties.name;
+      marker.addTo(feature.properties.confirmed ? confirmed : nonconfirmed);
+      oms.addMarker(marker);
+    }
   }
+  newEditMarker = L.marker(latLng, {
+    icon: L.icon({
+      iconUrl: './assets/new.png',
+      iconSize: [32, 44],
+      iconAnchor: [16, 44],
+      popupAnchor: [0, -44]
+    })
+  });
+
+  newEditMarker.addEventListener('remove', () => {
+    newEditMarker.setLatLng(mymap.getCenter());
+  });
+  newEditMarker.once("click", () => {
+    preparePoiPane();
+  });
+
+  mymap.on('moveend',() => {
+    if (!mymap.hasLayer(newEditMarker)) {
+      newEditMarker.setLatLng(mymap.getCenter());
+    }
+  });
+  oms.addListener("click", preparePoiPane);
+  layerControl = L.control.layers(null, {
+    "現況確認済み": confirmed,
+    "未確認(情報募集中)": nonconfirmed,
+    "新規報告ピン表示": newEditMarker
+  }, {
+    position: "bottomright"
+  }).addTo(mymap);
+  confirmed.addTo(mymap);
 });
-fetch(geoJson)
+/*fetch(geoJson)
   .then(async (data) => data.json())
   .then((geojson) => {
   //  console.log(geojson);
@@ -134,7 +196,7 @@ fetch(geoJson)
       position: "bottomright"
     }).addTo(mymap);
     confirmed.addTo(mymap);
-  });
+  });*/
 oms.addListener("spiderfy", (markers) => {
   spiderfyStatus = true;
   mymap.closePopup();
